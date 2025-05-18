7e9e24c45bb0d85cf6ba7c546338e23e2c2da634
Carlos Eduardo Witte <cewitte@icloud.com>
2025-05-18 09:34:11 -0300

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Carlos Eduardo Witte on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Carlos Eduardo Witte on 18/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
