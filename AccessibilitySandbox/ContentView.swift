753bea3646db237dd1ff19546d693be006bb31cd
Carlos Eduardo Witte <cewitte@icloud.com>
2025-05-18 17:38:41 -0300

    added accessibility code

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                selectedPicture = Int.random(in: 0...3)
+            } label: {
+                Image(pictures[selectedPicture])
+                    .resizable()
+                    .scaledToFit()
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            
+            
+            Image(decorative: "character")
+                .resizable()
+                .scaledToFit()
+                .accessibilityHidden(true)
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.largeTitle)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Your score is 1000"))
         }
-        .padding()
+        
     }
 }
 
